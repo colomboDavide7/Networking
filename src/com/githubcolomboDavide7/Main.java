5594389dcfffe6ef8d316050785afb7ff21b2f36
Davide Colombo <davide.colombo02@universitadipavia.it>
2020-11-19 22:48:19 +0100

    Added URL and URLConnection tests

--- src/com/githubcolomboDavide7/Main.java
+++ src/com/githubcolomboDavide7/Main.java
@@ -1,8 +1,27 @@
 package com.githubcolomboDavide7;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            Socket s = new Socket("whois.internic.net", 43);
 
+            InputStream in = s.getInputStream();
+            OutputStream out = s.getOutputStream();
+            String toKnow = "OraclePressBooks.com";
+            byte[] buffer = toKnow.getBytes();
+            out.write(buffer);
+            int c;
+            while((c = in.read()) != -1)
+                System.out.print((char) c);
+            s.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- test/ClientTest/ClientTest.java
+++ test/ClientTest/ClientTest.java
@@ -1,7 +1,14 @@
 package ClientTest;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalField;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -33,6 +40,45 @@ public class ClientTest {
         }
     }
 
-    
+    @Test
+    public void shouldCreateURL(){
+        System.out.println("* URL test: shouldCreateURL()\n");
+        try {
+            URL url = new URL("http", "www.HerbSchildt.com", "index.html");
+            assertEquals(-1, url.getPort());
+            assertEquals("http", url.getProtocol());
+            assertEquals("www.HerbSchildt.com", url.getHost());
+            assertEquals("index.html", url.getFile());
+        } catch(MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void shouldEstablishURLConnection(){
+        System.out.println("* URLConnection test: shouldEstablishURLConnection()\n");
+        try {
+            URL url = new URL("http://www.internic.net");
+            URLConnection conn = url.openConnection();
+
+            System.out.println("response date in millisecond from 1 January 1970: " + conn.getDate());
+            System.out.println("content-type: " + conn.getContentType());
+            System.out.println("last modified date: " + conn.getLastModified());
+
+            long len = conn.getContentLengthLong();
+            int c;
+            if(len != 0){
+                System.out.println("============= CONTENT =============");
+                InputStream in = conn.getInputStream();
+                while((c = in.read()) != -1)
+                    System.out.print((char) c);
+            }
+        } catch(MalformedURLException e) {
+            e.printStackTrace();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
